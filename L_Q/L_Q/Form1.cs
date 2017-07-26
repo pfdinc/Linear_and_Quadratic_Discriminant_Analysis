6127544a90598c9df1f9ae96073527119780befe
Pushkin Feleke <pfdinc@gmail.com>
2017-07-26 09:47:12 -0700

    Latest
    Min Max

--- L_Q/L_Q/Form1.cs
+++ L_Q/L_Q/Form1.cs
@@ -15,6 +15,56 @@ namespace L_Q
         public Form1()
         {
             InitializeComponent();
+            button1.Width = 200;
+            button2.Width = 200;
+            button3.Width = 200;
+            button4.Width = 200;
+        }
+
+
+        /// <summary>
+        /// this button will minamize and maximixe buttons and panel 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (panel2.Width == 240) {
+
+                button5.Text = "MAX";
+                panel2.Width = 50;
+
+                /// button width change 
+                button1.Width = 45;
+                button2.Width = 45;
+                button3.Width = 45;
+                button4.Width = 45;
+
+
+            } else if (panel2.Width == 50) {
+                button5.Text = "MIN";
+                panel2.Width = 240;
+
+                //button width change to 200 px 
+                button1.Width = 200;
+                button2.Width = 200;
+                button3.Width = 200;
+                button4.Width = 200;
+
+
+
+
+
+
+
+
+
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
